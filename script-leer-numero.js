f8f7a97b6421acc5fde75ecde50e065233475144
Juan-Rubio166 <jrubguz1407@g.educaand.es>
2025-02-03 12:08:43 +0100

    Update script-leer-numero.js

--- script-leer-numero.js
+++ script-leer-numero.js
@@ -1,1 +1,9 @@
+document.write ("<br>");
+var numero = prompt("introduce un numero del 1 al 100");
+numero = number(numero);
 
+while (numero<1 || numero >100) {
+  numero = prompt("numero invalido, vuelve a hacerlo");
+  numero = number (number);
+}
+document.write ("</br>");
