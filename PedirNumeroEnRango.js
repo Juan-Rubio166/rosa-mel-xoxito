951d738232e8eb7850fba699a39f55ac3ce6bd50
Juan-Rubio166 <jrubguz1407@g.educaand.es>
2025-02-10 12:15:26 +0100

    Update PedirNumeroEnRango.js

--- PedirNumeroEnRango.js
+++ PedirNumeroEnRango.js
@@ -1,1 +1,9 @@
+function PedirNumeroEnRango.js(min, max) {
+  var numero;
+  do {
+    numero = prompt("ingrese un número entre el " + min +" y " + max +":");
+    numero = number(numero);
+  } while (numero < min || numero > max);
 
+  return numero;
+}
