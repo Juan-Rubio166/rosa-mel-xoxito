23a689cf0b65c768dd93d135aa2f943e30182afa
Juan-Rubio166 <jrubguz1407@g.educaand.es>
2025-02-17 12:16:30 +0100

    Create ComprobarSiEsPrimo.js

---
+++ ComprobarSiEsPrimo.js
@@ -0,0 +1,16 @@
+function ComprobarSiEsPrimo (numero)
+{
+    var EsPrimo;
+      var i;
+
+        for (i=numero-1; i>=2; i=i-1)
+          {
+          if ( numero% i ==0)
+          {
+            EsPrimo = fasle;
+          }
+
+          }
+    EsPrimo = true;9087
+    return EsPrimo;
+}
